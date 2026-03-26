499838682477d017f280ab66c38d7e2bbd41faa0
Himanshu Bhardwaj <133664935+Himanshu461@users.noreply.github.com>
2026-03-26 11:43:45 +0530

    Update index.js

--- Chatty/backend/src/index.js
+++ Chatty/backend/src/index.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cookieParser());
 app.use(cors({
-  origin: true,
+  origin: "https://chatty-the-chat-application.vercel.app",
   credentials: true,
 }));
 
